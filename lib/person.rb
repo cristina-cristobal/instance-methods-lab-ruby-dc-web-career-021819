8732075df88fc7493a35ecd763a34bb3bcbb1313
github username <github email address>
2019-02-20 08:28:53 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  # Add an instance method #talk to your Person class in lib/person.rb that will puts "Hello World!"
+  def talk
+    puts "Hello World!"
+  end
+# Add an instance method #walk to your Person class in lib/person.rb that will puts "The Person is walking".
+  def walk
+    puts "The Person is walking"
+  end
+end
